75be911a60e0e56d05ce200fc787ba5a09a72f91
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-07-05 17:25:44 +0200

    Update zte_modem_util.py

--- custom_components/zte_lte_modem/zte_modem_util.py
+++ custom_components/zte_lte_modem/zte_modem_util.py
@@ -1,4 +1,4 @@
-from zte_modem_common import ZteModemConnection, parseSmsDate
+from zte_modem_common import ZteModemConnection
 
 import smsutil
 import json
@@ -20,51 +20,12 @@ def doGetLteStatus(connection):
     resp = connection.getLteStatus()
     print('zte_modem_util: doGetLteStatus: ', json.dumps(resp.json(), indent=4))
 
-def doGetAllSms(connection):
-    smsList = connection.getSms(False)
-
-    for sms in smsList:
-        print('zte_modem_util: doGetAllSms: SMS fields: ', json.dumps(sms, indent=4))
-        smsText = smsutil.decode(bytes.fromhex(sms['content']), encoding='utf_16_be')
-        print('zte_modem_util: doGetAllSms: SMS content: ', smsText)
-
-        smsDate = parseSmsDate(sms['date'])
-        print('zte_modem_util: doGetAllSms: SMS parsed date: ', smsDate)
-
-def doFetchSms(connection):
-    sms = connection.fetchSms()
-
-    if sms != None:
-        print('zte_modem_util: doFetchSms: SMS fields: ', json.dumps(sms, indent=4))
-        smsText = smsutil.decode(bytes.fromhex(sms['content']), encoding='utf_16_be')
-        print('zte_modem_util: doFetchSms: SMS content: ', smsText)
-    else:
-        print('zte_modem_util: no more SMS to read!')
-
-def doSendSms(connection):
-    recipient = input("Recipient number: \n")
-    message = input("Message to send: \n")
-    date = datetime.datetime.now()
-
-    resp = connection.sendSms(recipient, date, message)
-
-    if resp.json()['result'] == 'success':
-        print('zte_modem_util: doSendSms: SMS successfully sent.')
-    else:
-        print('zte_modem_util: doSendSms: failed to send sms: ', resp.json()['result'])
 
 def doCheckUser(connection):
     resp = connection.checkLoginStatus()
     print('zte_modem_util: doCheckUser: ', json.dumps(resp.json(), indent=4))
 
 
-connection = ZteModemConnection('http', 'localhost', '8254', 'admin', getpass())
+connection = ZteModemConnection('http', 'localhost', getpass())
 
 connection.login()
-
-#doFetchSms(connection)
-#doGetAllSms(connection)
-#doGetModemStatus(connection, "cell_id,lte_rsrp,signalbar,wan_active_band,spn_name_data")
-#doCheckUser(connection)
-
-doSendSms(connection)
